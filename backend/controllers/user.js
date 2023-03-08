87000f4d461b25015d271e0ad6a1f1cc83f054c5
JulienForesier <forestierjulien3@gmail.com>
2023-03-08 14:11:16 +0100

    fix mongoose

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,18 +5,23 @@ const User = require('../models/User')
 
 
 exports.signup = (req, res, next) => {
- bcrypt.hash(req.body.password, 10)
-     .then(hash => {
-         const user = new User({
-             email: req.body.email,
-             password: hash
-         });
-         user.save()
-             .then(() => res.status(201).json({message: 'Utilisateur créé !'}))
-             .catch(error => res.status(400).json({error}));
-     })
-     .catch(error => res.status(500).json({ error }));
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save(function (err) {
+                if (!err) {
+                    return res.status(201).json({message: 'Utilisateur créé !'})
+                } else {
+                    return res.status(400).json({err})
+                }
+            })
+        })
+        .catch(error => res.status(500).json({ error }));
 };
+
 
 exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
@@ -30,9 +35,9 @@ exports.login = (req, res, next) => {
                         return res.status(401).json({ error: 'Mot de passe incorrect !' });
                     }
                     res.status(200).json({
-                        userId: user.Id,
+                        userId: user._id,
                         token: jwt.sign(
-                            { userId: user.Id },
+                            { userId: user._id },
                             'RANDOM_TOKEN_SECRET',
                             { expiresIn: '24h' }
                         )
